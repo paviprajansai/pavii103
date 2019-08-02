43cc6bcbad4f890ae3a7cb3370ffb34b48655ead
paviprajansai <52271026+paviprajansai@users.noreply.github.com>
2019-08-01 18:35:21 -1200

    Create pavii105.py

---
+++ pavii105.py
@@ -0,0 +1,7 @@
+r11=['I','II','III','IV','V','VI','VII','VIII','IX','X','XI','XII','XIII','XIV','XV','XVI','XVII','XVIII','XIX','XX']
+s11=input()
+for i in range(0,21):
+	if s11==r11[i]:
+		print(i+1)
+		break
+    #i
